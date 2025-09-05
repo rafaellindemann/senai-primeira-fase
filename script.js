561a60c53a0c67d692acf5a1f79716b80110ac6a
rafaellindemann <rafaelld@gmail.com>
2025-09-04 22:38:20 -0300

    add html

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-alert("Olá mundo!")
+// alert("Olá mundo!")
+// alert("Oi")
+// alert("Tudo bem?")
+// alert("Você vem sempre aqui?")
+
+// let idade 
+
+// idade = 18
+// alert(idade)
+
+// idade = "melancia"
+// alert(idade)
+
+// idade = 18.1
+// alert(idade)
+
+const amor = "meu primeiro amor"
+alert(amor)
+
+alert(amor)
+let amor2 = "meu segundo amor"
+
+var antigao = "Nunca usar essa poioca"
 
 
