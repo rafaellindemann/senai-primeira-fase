9964ec3811e31472d056157e8d82dc25c7c795ab
rafaellindemann <rafaelld@gmail.com>
2025-09-10 22:30:08 -0300

    sq02

---
+++ sq02/ex01.js
@@ -0,0 +1,19 @@
+let idade
+idade = Number(prompt("Digite sua idade: ")) 
+if(idade >= 18){
+    alert("Maior de idade")
+    alert("Pode sair pro intervalo")
+}else{
+    alert("Dimenó")
+    alert("Pode sair pro recreio")
+}
+
+
+
+
+
+
+
+
+console.log(idade);
+
---
+++ sq02/ex04.js
@@ -0,0 +1,16 @@
+let numero
+numero = Number(prompt("Digite o número: "))
+if(numero % 2 == 0){
+    alert("par")
+}else{
+    alert("ímpar")
+}
+
+
+
+// ler um número
+
+// se o resto da divisao por 2 for 0//o número for par
+//     informar que é par
+// senão
+//     informar que é ímpar
