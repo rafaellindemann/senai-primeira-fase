9627264311d3e9158e817c92b9bffbad622ec5f2
rafaellindemann <rafaelld@gmail.com>
2025-09-15 22:11:59 -0300

    add ex 5.2

--- sq05/ex52.js
+++ sq05/ex52.js
@@ -1,8 +1,6 @@
 let A, B
 A = Number(prompt("Digite o primeiro número:"))
 B = Number(prompt("Digite o segundo número:"))
-
-// alert(A+B) 1+1 = 11
 if(A > B){
     alert("Primeiro: "+ A )
     // alert("Primeiro ("+ A +") é maior que o segundo ("+ B +")")
