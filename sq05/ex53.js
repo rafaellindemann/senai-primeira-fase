e745e768fe44f51a19496caee2e4a7933b66c2c4
rafaellindemann <rafaelld@gmail.com>
2025-09-17 20:16:26 -0300

    add 5.3

---
+++ sq05/ex53.js
@@ -0,0 +1,31 @@
+let placa
+placa = Number(prompt("Último número: "))
+
+if(placa == 0 || placa == 1){
+    alert("Não pode rodar na segunda-feira")
+}else if(placa == 2 || placa == 3){
+    alert("Não pode rodar na terça-feira")
+}else if(placa == 4 || placa == 5){
+    alert("Não pode rodar na quarta-feira")
+}else if(placa == 6 || placa == 7){
+    alert("Não pode rodar na quinta-feira")
+}else if(placa == 8 || placa == 9){
+    alert("Não pode rodar na sexta-feira")
+}else {
+    alert("Sai do celular e digita direito")
+}
+
+
+
+
+
+// if(placa == 0 || placa == 1){
+    // if(2 == 0 || 2 == 1){
+        // if(f || f){
+            // if(f){
+                
+            
+// if(placa == 0 || placa == 1){
+// if(1 == 0 || 1 == 1){
+// if(f || v){
+// if(v){
