889b613c488142bb46ba485a0064a0354ab0e3fb
rafaellindemann <rafaelld@gmail.com>
2025-09-29 22:25:15 -0300

    add while

---
+++ repeticoes/script.js
@@ -0,0 +1,16 @@
+
+// let acertos = Number(prompt("Quantos acertos?"))
+// let valorPorQuestao = 10/16
+// let nota = acertos * valorPorQuestao
+
+// alert("Sua nota: " + nota)
+
+
+
+let contador = 0
+while(contador < 2){
+    // alert("Contador: " + contador)
+    contador = contador + 1
+    console.log("Contador: " + contador)
+}
+
