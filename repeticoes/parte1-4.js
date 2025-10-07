82682575c5c24bc74d542c784ab4998c5b14d3d6
rafaellindemann <rafaelld@gmail.com>
2025-10-06 21:57:24 -0300

    add exercícios

--- repeticoes/parte1-4.js
+++ repeticoes/parte1-4.js
@@ -1,11 +1,30 @@
-
+//parte 1
+//ex4
 // for(let i=1; i<=10; i++){
 //     let resultado = i * 5
 //     console.log(i + "x" + 5 + "=" + resultado);
 // }
 
-let numero = Number(prompt("Tabuada do quê?"))
-for(let i=1; i<=10; i++){
-    let resultado = i * numero
-    console.log(i + "x" + numero + "=" + resultado);
+//ex5
+// let numero = Number(prompt("Tabuada do quê?"))
+// for(let i=1; i<=10; i++){
+//     let resultado = i * numero
+//     console.log(i + "x" + numero + "=" + resultado);
+// }
+
+//ex6 mais bonitinho
+for(let i=1; i<=20; i++){
+    if(i%2 == 1){
+        alert(i)
+    }
 }
+// ex6b-feio
+// for(let i=1; i<=20; i=i+2){
+//     alert(i)
+// }
+
+// ex6b-otrojeito
+// for(let i=1; i<=20; i++){
+//     alert(i)
+//     i++
+// }
