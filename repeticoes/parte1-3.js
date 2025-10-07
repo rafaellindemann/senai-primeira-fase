ed651d82db1d285e80492abc0417a24adddf7a09
rafaellindemann <rafaelld@gmail.com>
2025-10-06 21:41:17 -0300

    add exercícios de repetições

---
+++ repeticoes/parte1-3.js
@@ -0,0 +1,23 @@
+// 🐱‍🏍Calcule a soma dos números de 1 a 100.
+
+
+let soma = 0
+console.log(soma);
+for(let i=1; i<=100; i++){
+    soma = soma + i
+    console.log(soma);
+}
+
+// let fat = 1
+// console.log(fat);
+// for(let i=1; i<=100; i++){
+//     fat = fat * i
+//     console.log(fat);
+// }
+
+
+// let i=1
+// while(i<=100){
+//         console.log(i);
+//     i++
+// }
---
+++ repeticoes/parte1-4.js
@@ -0,0 +1,11 @@
+
+// for(let i=1; i<=10; i++){
+//     let resultado = i * 5
+//     console.log(i + "x" + 5 + "=" + resultado);
+// }
+
+let numero = Number(prompt("Tabuada do quê?"))
+for(let i=1; i<=10; i++){
+    let resultado = i * numero
+    console.log(i + "x" + numero + "=" + resultado);
+}
