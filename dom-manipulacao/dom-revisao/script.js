6400932cc5d783f2de76d066c01785ebaa1ad5fe
rafaellindemann <rafaelld@gmail.com>
2025-10-23 22:21:29 -0300

    add wip bc1014

--- dom-manipulacao/dom-revisao/script.js
+++ dom-manipulacao/dom-revisao/script.js
@@ -4,6 +4,8 @@ function cadastrarUsuario(){
     // console.log("até aqui deu");
     usuario = document.getElementById("inputUsuario").value
 
-    document.getElementById("inputUsuario").value = ' '
+    document.getElementById("inputUsuario").value = ''
+
+    document.getElementById("resultado").innerHTML = "Usuário cadastrado com sucesso: " + usuario
     
 }
