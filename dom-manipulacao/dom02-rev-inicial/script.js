693a6b20f2d6f69307e49153ef4c3e5df81168c0
rafaellindemann <rafaelld@gmail.com>
2025-10-27 20:20:39 -0300

    add rev demo dom e flex

---
+++ dom-manipulacao/dom02-rev-inicial/script.js
@@ -0,0 +1,7 @@
+function executarTeste(){
+    let nome = document.getElementById("inputNome").value
+    // console.log(nome);
+
+    document.getElementById('resultado').innerHTML += nome    
+    
+}
