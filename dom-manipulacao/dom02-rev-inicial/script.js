14d41c32e151acdce5a8c45b2e0182cee3969e7e
rafaellindemann <rafaelld@gmail.com>
2025-10-27 20:31:55 -0300

    add rev demo dom e flex

--- dom-manipulacao/dom02-rev-inicial/script.js
+++ dom-manipulacao/dom02-rev-inicial/script.js
@@ -1,3 +1,13 @@
+function calcularEx1014(){
+    let distancia = Number( document.getElementById('inputDistancia').value )
+    let combustivel = Number( document.getElementById('inputCombustivel').value )
+
+    let consumo = distancia / combustivel
+    console.log(consumo);
+    
+}
+
+
 function executarTeste(){
     let nome = document.getElementById("inputNome").value
     // console.log(nome);
