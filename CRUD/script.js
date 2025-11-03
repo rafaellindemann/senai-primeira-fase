8bf0189d1e9e691c1b5a64047a674e470c5f9736
rafaellindemann <rafaelld@gmail.com>
2025-11-03 20:27:15 -0300

    add cadastro dino

---
+++ CRUD/script.js
@@ -0,0 +1,37 @@
+
+// const dinossauro = {
+//     nome: 'Rex',
+//     altura: '6'
+// }
+
+const dinossauros = []
+
+// dinossauros.push(dinossauro)
+
+// document.getElementById('dino').innerHTML = `Nome: ${dinossauros[0].nome}; Altura: ${dinossauros[0].altura}`
+
+function cadastrarDino(){
+    let nomeLido = document.getElementById('inputNome').value
+    let alturaLida = Number(document.getElementById('inputAltura').value)
+
+    const dino = {
+        nome: nomeLido,
+        altura: alturaLida
+    }
+    dinossauros.push(dino)
+
+    console.log(dinossauros);
+    
+    limparForm()
+    alert("Dino cadastrado com sucesso!")
+    
+}
+
+function limparForm(){
+    document.getElementById('inputNome').value = ''
+    document.getElementById('inputAltura').value = ''
+    // document.getElementById('inputPesoOuMassa').value = ''
+    
+    document.getElementById('inputNome').focus()
+}
+
