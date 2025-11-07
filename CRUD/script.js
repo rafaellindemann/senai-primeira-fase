86e55b0fc18c874b1794f84c09c6274c696b06d4
rafaellindemann <rafaelld@gmail.com>
2025-11-06 21:53:04 -0300

    add leitura de registros do crud e navegação via js

--- CRUD/script.js
+++ CRUD/script.js
@@ -7,6 +7,7 @@
 // }
 
 const dinossauros = []
+// let dinossauros = []
 
 // dinossauros.push(dinossauro)
 
@@ -26,6 +27,8 @@ function cadastrarDino(){
     
     limparForm()
     alert("Dino cadastrado com sucesso!")
+
+    mostrarTodosDinos()
     
 }
 
@@ -37,3 +40,35 @@ function limparForm(){
     document.getElementById('inputNome').focus()
 }
 
+function mostrarTodosDinos(){
+    document.getElementById('listaDinos').innerHTML = '' 
+    for(let i=0; i<dinossauros.length ; i++){
+        // console.log(i);
+        document.getElementById('listaDinos').innerHTML += `
+            <div class='card'>
+                <h3>${dinossauros[i].nome} </h3>
+                <p>Altura: ${dinossauros[i].altura}</p>
+            </div>
+        `
+        
+    }
+}
+
+function lancarMeteoro(){
+    dinossauros.length = 0
+    mostrarTodosDinos()
+}
+
+function navegarParaDois(){
+    // alert('Tchau')
+
+    let nome = prompt("User:")
+    let senha = prompt("Senha: ")
+
+    if(nome == "admin" && senha == "admin"){
+        window.location.href = 'dois.html'
+    }else{
+        alert("Você está tentando invadir, a polícia tá chegando!!")
+    }
+
+}
