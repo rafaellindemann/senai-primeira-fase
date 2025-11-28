564d56b30dbeca2074796d137ef648df39b4cc70
rafaellindemann <rafaelld@gmail.com>
2025-11-27 21:16:01 -0300

    add edição do registro

--- CRUD/script.js
+++ CRUD/script.js
@@ -6,7 +6,9 @@
 //     altura: '6'
 // }
 
-const dinossauros = JSON.parse(localStorage.getItem("dinossauros")) || []
+let dinossauros = JSON.parse(localStorage.getItem("dinossauros")) || []
+mostrarTodosDinos()
+let idEditando = 0
 
 
 // dinossauros.push(dinossauro)
@@ -14,10 +16,14 @@ const dinossauros = JSON.parse(localStorage.getItem("dinossauros")) || []
 // document.getElementById('dino').innerHTML = `Nome: ${dinossauros[0].nome}; Altura: ${dinossauros[0].altura}`
 
 function cadastrarDino(){
+
+    dinossauros = JSON.parse(localStorage.getItem("dinossauros")) || []
+
     let nomeLido = document.getElementById('inputNome').value
     let alturaLida = Number(document.getElementById('inputAltura').value)
 
     const dino = {
+        id: Date.now(),
         nome: nomeLido,
         altura: alturaLida
     }
@@ -46,6 +52,7 @@ function limparForm(){
 }
 
 function mostrarTodosDinos(){
+    dinossauros = JSON.parse(localStorage.getItem('dinossauros')) || []
     document.getElementById('listaDinos').innerHTML = '' 
     for(let i=0; i<dinossauros.length ; i++){
         // console.log(i);
@@ -53,14 +60,48 @@ function mostrarTodosDinos(){
             <div class='card'>
                 <h3>${dinossauros[i].nome} </h3>
                 <p>Altura: ${dinossauros[i].altura}</p>
+                <p>ID🤫: ${dinossauros[i].id}</p>
+                <button onclick="editarDino(${dinossauros[i].id})">📝</button>
             </div>
         `
         
     }
 }
+function editarDino(id){ // selecionarDino seria um nome melhor
+    console.log(id);
+    dinossauros = JSON.parse(localStorage.getItem('dinossauros')) || []
+
+    for(let i=0; i<dinossauros.length; i++){
+        if(dinossauros[i].id == id){
+            console.log(dinossauros[i]);
+            document.getElementById('inputNome').value = dinossauros[i].nome
+            document.getElementById('inputAltura').value = dinossauros[i].altura
+            idEditando = dinossauros[i].id
+        }
+    }
+    
+}
+
+function salvarDino(){
+    dinossauros = JSON.parse(localStorage.getItem('dinossauros')) || []
+    for(let i=0; i<dinossauros.length; i++){
+        if(dinossauros[i].id == idEditando){
+            console.log(dinossauros[i]);
+            dinossauros[i].nome = document.getElementById('inputNome').value
+            dinossauros[i].altura = document.getElementById('inputAltura').value
+        }
+    }
+    localStorage.setItem("dinossauros", JSON.stringify(dinossauros))
+    mostrarTodosDinos()
+    alert("Alterado com muito sucesso")
+    limparForm()
+    idEditando = 0
+}
 
 function lancarMeteoro(){
+    dinossauros = JSON.parse(localStorage.getItem('dinossauros')) || []
     dinossauros.length = 0
+    localStorage.setItem("dinossauros", JSON.stringify(dinossauros))
     mostrarTodosDinos()
 }
 
@@ -76,4 +117,15 @@ function navegarParaDois(){
         alert("Você está tentando invadir, a polícia tá chegando!!")
     }
 
+}
+
+
+function testar(id){
+    for(let i=0; i<dinossauros.length; i++){
+        if(dinossauros[i].id == id){
+            console.log(dinossauros[i]);
+
+        }
+    }
+    
 }
